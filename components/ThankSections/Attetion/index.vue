9e3d794cbab3f6ab08f487d5f86c4af45d2862c7
Samuel Leonel <samueleonelfreitas@hotmail.com>
2020-12-28 15:38:06 -0300

    Criação da seção de attetion

---
+++ components/ThankSections/Attetion/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="container">
+    <h1 class="title">
+      Quase lá
+    </h1>
+    <p class="text">
+      Você está a um passo de garantir a sua participação no evento:
+    </p>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import './styles.scss';
+</style>
--- pages/obrigado.vue
+++ pages/obrigado.vue
@@ -1,13 +1,16 @@
 <template>
   <div>
+    <Attetion />
     <Footer />
   </div>
 </template>
 
 <script>
 import Footer from '@/components/IndexSections/Footer'
+import Attetion from '@/components/ThankSections/Attetion'
 export default {
   components: {
+    Attetion,
     Footer
   }
 }
